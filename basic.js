52d5901d270a4a79a3d56f27324433ae6bc006e6
rakeshlande <rakeshlande2155@gmail.com>
2024-04-05 21:50:40 +0530

    crop

---
+++ basic.js
@@ -0,0 +1,57 @@
+// console.log("i am student");
+// var name=prompt("i am taking input");
+
+// console.log("name");
+
+
+// for(let i=1;i<=10;i++){
+//     console.log(i*2);
+// }
+
+// let n=1;
+// while(n<=10)
+// {
+//     console.log(n);
+//     n++;
+// }
+
+
+// let n=prompt("give me number");
+// if(n%2==0)
+// {
+//     console.log("i am even");
+// }else{
+//     console.log("i am odd");
+// }
+
+// let marks=prompt("enter your marks")
+// if(makrs=>80 && marks<=90 )
+// {
+//     console.log("a grade");
+// }else if (marks=>70 && marks<=80)
+// {
+//     console.log("b grade");
+// }
+// else if (marks=>60 && marks<=70)
+// {
+//     console.log("c grade");
+// }
+// else
+// {
+//     console.log("d grade")
+// }
+
+// 
+
+const favmovie="avengers";
+let guess=prompt("guess any movie");
+while((guess!=favmovie) &&  (guess=   ="quit"))
+{
+    guess=prompt("wrong answer!!! enter movies again");
+}
+if (guess==favmovie)
+{console.log("yes correct");
+}
+else{
+    console.log("you are quited");
+}
